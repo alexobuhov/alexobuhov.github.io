2b3f03c0e3139160c2bad75d247a7cfb460645a5
alex <pfreg@mail.ru>
2020-04-29 19:44:25 +0500

    add first section

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ $(document).ready(function () {
     $("body").toggleClass("lock");
   });
 });
+
+$(document).ready(function () {
+  $(".header__link").click(function () {
+    $(".header__burger, .header__menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+});
+
+$(window).scroll(function () {
+  $(".picture").css(
+    "background-position",
+    "0% " + ($(this).scrollTop() / 15 - 10) + "px"
+  );
+});
